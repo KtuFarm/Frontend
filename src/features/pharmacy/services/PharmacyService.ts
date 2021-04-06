3594202937658658904f9627a52842e2033bb645
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-04-06 11:44:16 +0300

    Add constants file

--- src/features/pharmacy/services/PharmacyService.ts
+++ src/features/pharmacy/services/PharmacyService.ts
@@ -1,19 +1,14 @@
+import { API_HOST, DEFAULT_HEADERS } from '../../../constants';
 import { PharmacyDTO } from '../dtos/PharmacyDTO';
 
 export const createPharmacy = async (
   pharmacy: PharmacyDTO
 ): Promise<boolean> => {
-  const response = await fetch(
-    `${process.env.REACT_APP_API_HOST}/api/v1/Pharmacies`,
-    {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Api-Request': 'true',
-      },
-      body: JSON.stringify(pharmacy),
-    }
-  );
+  const response = await fetch(`${API_HOST}/api/v1/Pharmacies`, {
+    method: 'POST',
+    headers: DEFAULT_HEADERS,
+    body: JSON.stringify(pharmacy),
+  });
 
   if (response.status !== 201) {
     throw new Error('Failed to create pharmacy');
