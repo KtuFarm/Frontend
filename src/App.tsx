059a5622aa2bc7402205f7d5c5b10ba368f68787
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-04-06 17:38:10 +0300

    Add pharmacy list fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import { Home } from 'features/home';
 import { Login } from 'features/login';
-import { CreatePharmacy, PharmacyList } from 'features/pharmacy';
+import { CreatePharmacy, Pharmacies } from 'features/pharmacy';
 
 export const App = (): JSX.Element => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/home" element={<Home />} />
-      <Route path="/pharmacy" element={<PharmacyList />} />
+      <Route path="/pharmacy" element={<Pharmacies />} />
       <Route path="/pharmacy/new" element={<CreatePharmacy />} />
     </Routes>
   );
--- src/features/pharmacy/index.ts
+++ src/features/pharmacy/index.ts
@@ -1,2 +1,2 @@
 export * from './CreatePharmacy';
-export * from './PharmacyList';
+export * from './Pharmacies';
