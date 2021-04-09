23ff3c5bc25b5784f9145aae3623f2cb77e389e3
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-04-09 13:45:11 +0300

    Fix header links

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,7 +5,7 @@ export const Header = (): JSX.Element => {
     <header className="text-gray-600 bg-white shadow body-font">
       <div className="container flex flex-col flex-wrap items-center p-5 mx-auto md:flex-row">
         <Link
-          to="/home"
+          to="/"
           className="flex items-center mb-4 font-medium text-gray-900 title-font md:mb-0"
         >
           <span className="ml-3 text-xl">Vaistukai</span>
@@ -19,7 +19,7 @@ export const Header = (): JSX.Element => {
           </Link>
         </nav>
         <Link
-          to="/"
+          to="/login"
           className="inline-flex items-center px-3 py-1 mt-4 text-base bg-gray-100 border-0 rounded focus:outline-none hover:bg-gray-200 md:mt-0"
         >
           Atsijungti
