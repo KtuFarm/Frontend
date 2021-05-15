32befb4c38e1b0c98177def2cbc358652b86c7b4
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-05-15 17:01:41 +0300

    Fix authentication

--- src/features/login/services/AuthService.ts
+++ src/features/login/services/AuthService.ts
@@ -1,11 +1,11 @@
 import { LoginDTO } from 'swagger/models';
 
-import { API_HOST, DEFAULT_HEADERS } from '../../../constants';
+import { API_HOST, DEFAULT_HEADERS_WITHOUT_AUTH } from '../../../constants';
 
 export const login = async (login: LoginDTO): Promise<Response> => {
   return fetch(`${API_HOST}/api/v1/Users/login`, {
     method: 'POST',
-    headers: DEFAULT_HEADERS,
+    headers: DEFAULT_HEADERS_WITHOUT_AUTH,
     body: JSON.stringify(login),
   });
 };
@@ -13,7 +13,10 @@ export const login = async (login: LoginDTO): Promise<Response> => {
 export const me = async (token: string): Promise<Response> => {
   return fetch(`${API_HOST}/api/v1/Users/me`, {
     method: 'GET',
-    headers: { ...DEFAULT_HEADERS, Authorization: `Bearer ${token}` },
+    headers: {
+      ...DEFAULT_HEADERS_WITHOUT_AUTH,
+      Authorization: `Bearer ${token}`,
+    },
     body: JSON.stringify(login),
   });
 };
