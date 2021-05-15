e22404dd6259c0986c795a08bd4770fbe9733b08
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-05-15 21:21:33 +0300

    Add order preparing

--- src/features/order/services/OrderService.tsx
+++
@@ -1,9 +0,0 @@
-import { API_HOST, DEFAULT_HEADERS } from '../../../constants';
-
-export const createOrder = async (order: unknown): Promise<Response> => {
-  return fetch(`${API_HOST}/api/v1/Orders`, {
-    method: 'POST',
-    headers: DEFAULT_HEADERS,
-    body: JSON.stringify(order),
-  });
-};
