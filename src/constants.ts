009f3210ce1f80aeb6d6f8700ed8ec973ac0b19e
Lukas Rakauskas <rakauskaslukas123@gmail.com>
2021-05-16 21:36:20 +0300

    Add origin and accept to default headers

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,8 @@
 export const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
   'X-Api-Request': 'true',
+  Origin: window.location.origin,
+  Accept: '*/*',
 };
 
 export const API_HOST = process.env.REACT_APP_API_HOST;
